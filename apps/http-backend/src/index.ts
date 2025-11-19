eb0cf4ca3ad673a2fcc19f7163730c89357e633a
gaurangAgarwal <gaurangag18@gmail.com>
2025-11-19 23:27:03 +0530

    next step

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,23 @@
 import express from "express";
 const app = express();
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middelware } from "./middleware";
+app.post("/signup",(req,res) => {
+
+})
+app.post("/signup",(req,res) => {
+const userId=1;
+const token = jwt.sign({
+  userId  
+},JWT_SECRET);
+res.json({
+    token
+})
+})
+app.post("/rooms",middelware,(req,res) => {
+res.json({
+  
+})
+})
 app.listen(3000);
